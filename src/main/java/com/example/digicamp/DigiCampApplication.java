1812103f23ec7c6ec8fc4f519ca28c59486780a7
Khushal Abrol <khushalabrol14@gmail.com>
2022-04-09 23:17:32 +0530

    Initial Setup

---
+++ src/main/java/com/example/digicamp/DigiCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.digicamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digicamp/DigiCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digicamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
